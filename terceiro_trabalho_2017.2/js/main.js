91abc6e90c6727a57889d97542aedc586e0c963f
João Victor Davim <joaovictor_rj@poli.ufrj.br>
2017-11-14 00:46:33 -0200

    Init rotate
    Function rotate declared. Obj texture resolution changed to power of 2. Code cleared.

--- terceiro_trabalho_2017.2/js/main.js
+++ terceiro_trabalho_2017.2/js/main.js
@@ -17,7 +17,8 @@ function init() {
 	container = document.createElement( 'div' );
 	document.body.appendChild( container );
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-	camera.position.z = 250;
+	camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
+	camera.position.z = 500;
 
 
 	// Scene
@@ -31,22 +32,11 @@ function init() {
 
 	// Texture
 	var manager = new THREE.LoadingManager();
-	manager.onProgress = function ( item, loaded, total ) {
-		console.log( item, loaded, total );
-	};
 	var textureLoader = new THREE.TextureLoader( manager );
 	var texture = textureLoader.load( 'textures/UV_Grid_Sm.jpg' );
 
 
 	// Model
-	var onProgress = function ( xhr ) {
-		if ( xhr.lengthComputable ) {
-			var percentComplete = xhr.loaded / xhr.total * 100;
-			console.log( Math.round(percentComplete, 2) + '% downloaded' );
-		}
-	};
-	var onError = function ( xhr ) {
-	};
 	var loader = new THREE.OBJLoader( manager );
 	loader.load( 'obj/male02/male02.obj', function ( object ) {
 		object.traverse( function ( child ) {
@@ -54,10 +44,9 @@ function init() {
 				child.material.map = texture;
 			}
 		} );
-		object.position.y = - 95;
 		toy = object;
 		scene.add( object );
-	}, onProgress, onError );
+	 });
 
 
 	// Render
@@ -67,7 +56,7 @@ function init() {
 	container.appendChild( renderer.domElement );
 
 
-	// Event Listener
+	// Resize Event Listener
 	window.addEventListener( 'resize', onWindowResize, false );
 
 	// Mouse Event Listener
@@ -82,15 +71,15 @@ function init() {
 		mouseY = event.clientY;
 	}
 
-	renderer.domElement.addEventListener('wheel', onMouseWheel, false); // Chrome, IE9, Safari, Opera
+	renderer.domElement.addEventListener( 'wheel', onMouseWheel, false ); // Don't work on Firefox
 
-	renderer.domElement.addEventListener ('mousedown', function () {
+	renderer.domElement.addEventListener ( 'mousedown', function () {
 		setMouse();
 		mouseIsPressed = true;
 		if (typeof mousePressed !== 'undefined') mousePressed();
-	});
+	} );
 
-	renderer.domElement.addEventListener ('mousemove', function () { 
+	renderer.domElement.addEventListener ( 'mousemove', function () { 
 		pmouseX = mouseX;
 		pmouseY = mouseY;
 		setMouse();
@@ -98,18 +87,13 @@ function init() {
 			if (typeof mouseDragged !== 'undefined') mouseDragged(); 
 		}
 		if (typeof mouseMoved !== 'undefined') mouseMoved();
-	});
+	} );
 
-	renderer.domElement.addEventListener ('mouseup', function () { 
+	renderer.domElement.addEventListener ( 'mouseup', function () { 
 		mouseIsPressed = false; 
 		if (typeof mouseReleased !== 'undefined') mouseReleased(); 
-	});
+	} );
 
-	renderer.domElement.addEventListener('dblclick', function () {
-		setMouse();
-		mouseIsPressed = false;
-		if (typeof dblClick != 'undefined') dblClick();
-	});
 }
 
 function onWindowResize() {
@@ -120,7 +104,7 @@ function onWindowResize() {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-function onMouseWheel(){
+function onMouseWheel() {
 	var e = window.event || e;
 	var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
 	toy.translateZ(delta*20); // Adjust zoom sensibility here
@@ -133,19 +117,23 @@ function animate() {
 }
 
 function render() {
-	camera.lookAt( new THREE.Vector3(0,0,0) );
 	renderer.render( scene, camera );
 }
 
-function translate(){
+function translate() {
 	var delta = new THREE.Vector3();
-	delta.subVectors(new THREE.Vector3(mouseX,mouseY,0), new THREE.Vector3(pmouseX,pmouseY,0));
-	toy.translateX(delta.x);
-	toy.translateY(-delta.y);
+	var mouse = new THREE.Vector3( mouseX, mouseY, 0 );
+	delta.subVectors( mouse, new THREE.Vector3( pmouseX, pmouseY, 0 ) );
+	toy.translateX( delta.x) ;
+	toy.translateY( -delta.y );
+}
+
+function rotate() {
+	
 }
 
 
 // Mouse functions
-function mouseDragged(){
+function mouseDragged() {
 	translate();
 }
